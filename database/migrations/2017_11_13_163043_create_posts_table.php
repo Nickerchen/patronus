1b6965eef4d2a220f111d4b4c41039e00f79d564
Mainau <felix.thcilhcs@gmail.com>
2017-12-13 18:05:09 +0100

    mini emoji bug behoben

--- database/migrations/2017_11_13_163043_create_posts_table.php
+++ database/migrations/2017_11_13_163043_create_posts_table.php
@@ -17,7 +17,7 @@ class CreatePostsTable extends Migration
           $table->increments('id');
           $table->text('body');
           $table->integer('user_id');
-          $table->integer('emotion');
+          $table->integer('emotion')->default(-100);
           $table->timestamps();
         });
     }
